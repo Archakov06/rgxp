cf7e7f2cc5748dcb7303383f4ac89d36d614b6be
Archakov Dennis <hello@archakov.im>
2017-09-18 16:42:23 +0300

    Update index.js

--- src/client/js/store/index.js
+++ src/client/js/store/index.js
@@ -6,7 +6,7 @@ export default function configureStore(initialState) {
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers');
+      const nextRootReducer = rootReducer;
       store.replaceReducer(nextRootReducer);
     });
   }
